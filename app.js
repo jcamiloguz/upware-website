9848ae4119eb2e2f590335cb63e8b671778a4628
jcamiloguzman <jcamiloguz1@gmail.com>
2020-05-25 11:52:44 -0500

    Responsive at 600px almost complete, menuless

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+// Boton Menu
+$("#menu").click(function () {
+  var visible = $("#menu-light").data("view");
+
+  if (visible == false) {
+    //$("#menu-header").css("right", 0);
+    $("#menu-light").animate({ right: 0 }, 500);
+    $("#menu-light").data("view", true);
+  }
+
+  if (visible == true) {
+    //$("#menu-header").css("right", 100 + "vh");
+    $("#menu-light").animate({ right: 100 + "vh" }, 500);
+    $("#menu-light").data("view", false);
+  }
+});
+
   var cw = $("#user1").width();
   var cw2 = $("#user2").width();
   $("#user1").css({
@@ -57,8 +74,8 @@ $(document).ready(function () {
           slidesToShow: 1,
           slidesToScroll: 1,
         },
-      },],
-
+      },
+    ],
   });
 });
 $(window).resize(function () {
@@ -68,15 +85,29 @@ $(document).ready(function () {
   resizeLogo();
 });
 
-
-  function resizeLogo(){
-    setTimeout(function(){  
-    
+function resizeLogo() {
+  setTimeout(function () {
     $width1 = $("#user1").width();
     $width2 = $("#user2").width();
-    $("#user1").css("height", $width1 );
-    $("#user2").css("height", $width2 );
-}, 100);
-    
-    
-    }
+    $("#user1").css("height", $width1);
+    $("#user2").css("height", $width2);
+  }, 100);
+}
+
+
+$(document).ready(function () {
+  
+});
+
+$(window).resize(function () {
+  var ventana = $(window).width();
+
+  if (ventana > 600) {
+    $("#menu-header").css("right", 0);
+    $("#menu-header").data("view", false);
+  }
+  if (ventana <= 600) {
+    $("#menu-header").css("right", 100 + "vh");
+    $("#menu-header").data("view", false);
+  }
+});
