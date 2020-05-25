2c563244b5fa89b8081ccd6112278486686aa557
jcamiloguzman <jcamiloguz1@gmail.com>
2020-05-24 21:36:33 -0500

    buttoms hovers, slick and slick responsive

---
+++ app.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+  var cw = $("#user1").width();
+  var cw2 = $("#user2").width();
+  $("#user1").css({
+    height: cw + "px",
+  });
+  $("#user2").css({
+    height: cw2 + "px",
+  });
+  $("#places").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    autoplaySpeed: 10,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+  $(".persons").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },],
+
+  });
+});
+$(window).resize(function () {
+  resizeLogo();
+});
+$(document).ready(function () {
+  resizeLogo();
+});
+
+
+  function resizeLogo(){
+    setTimeout(function(){  
+    
+    $width1 = $("#user1").width();
+    $width2 = $("#user2").width();
+    $("#user1").css("height", $width1 );
+    $("#user2").css("height", $width2 );
+}, 100);
+    
+    
+    }
